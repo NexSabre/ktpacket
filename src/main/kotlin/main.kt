8e5173a373af5551d242c387379aa3e6d26c47d4
Nex Sabre <NexSabre@users.noreply.github.com>
2021-08-26 13:04:40 +0200

    POC send

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,36 @@
 import templates.Ether
+import templates.IP
+import templates.TCP
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
 
 fun main(args: Array<String>) {
     val ether = Ether(
         "00:00:00:00:00:00",
         "ff:ff:ff:ff:ff:ff",
         "0x9000"
-    )
-    println(ether.dst)
-    println(ether.bin())
-    println(ether.hex())
-}
+    ) / IP() / TCP()
+//    println(ether.dst)
+//    println(ether.bin())
+//    println(ether.hex())
 
+    val socket = DatagramSocket(8888)
+    socket.broadcast = true
+    println(ether.bin())
+    println(ether.bin().toByteArray())
+    val packet = DatagramPacket(ByteArray(2048), ByteArray(2048).size)
+    socket.send(
+        DatagramPacket(
+            ether.bin().toByteArray(),
+            ether.bin().toByteArray().size,
+            InetAddress.getByName("127.0.0.1"),
+            8888
+        )
+    )
+    socket.receive(packet)
+    println("data:" + String(packet.data).takeWhile { it.toInt() != 0 })
+    println(String(packet.data).takeWhile { it.toInt() != 0 } == ether.bin())
+}
 
 
